4fff0c68e79ec834d4efc8efab5b94a58da57847
blancowendell <blanco.wendell15@gmail.com>
2025-02-27 03:06:38 +0800

    ADD LOAD STATION PORTAL

---
+++ routes/LoadIndex.js
@@ -0,0 +1,28 @@
+const mysql = require("./repository/lagonaDb");
+//const moment = require('moment');
+var express = require("express");
+const { LoadStationValidator } = require("./controller/middleware");
+const {
+  JsonErrorResponse,
+  JsonSuccess,
+  JsonWarningResponse,
+  MessageStatus,
+  JsonDataResponse,
+} = require("./repository/response");
+const { InsertTable, Select, Update } = require("./repository/dbconnect");
+const {
+  SelectStatement,
+  InsertStatement,
+  UpdateStatement,
+} = require("./repository/customhelper");
+const { DataModeling } = require("./model/lagonaDb");
+var router = express.Router();
+//const currentDate = moment();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  LoadStationValidator(req, res, "LoadIndexLayout");
+});
+
+
+module.exports = router;
